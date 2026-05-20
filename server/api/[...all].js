02f7cf2f8cbfd1fd531777b87d741f3582179557
Praveen Bommu <praveenbommu5@gmail.com>
2026-05-20 16:21:29 +0530

    updated apis for server

--- server/api/[...all].js
+++
@@ -1,3 +0,0 @@
-const appModule = require('../dist/app');
-
-module.exports = appModule.default || appModule;
--- server/src/app.ts
+++ server/src/app.ts
@@ -2,7 +2,6 @@ import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import swaggerUi from 'swagger-ui-express';
 import routes from './routes/index';
 import { errorHandler } from './middleware/errorHandler';
 import { swaggerDocument } from './config/swagger';
@@ -42,7 +41,42 @@ app.get('/api/api-docs.json', (_req, res) => {
   res.json(swaggerDocument);
 });
 
-app.use('/api/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get('/api/api-docs', (_req, res) => {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.send(`
+    <!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Tea Time API Docs</title>
+        <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" />
+        <style>
+          html { box-sizing: border-box; overflow-y: scroll; }
+          *, *:before, *:after { box-sizing: inherit; }
+          body { margin: 0; background: #f7f7f7; }
+          #swagger-ui { max-width: 1400px; margin: 0 auto; }
+        </style>
+      </head>
+      <body>
+        <div id="swagger-ui"></div>
+        <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js" crossorigin></script>
+        <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-standalone-preset.js" crossorigin></script>
+        <script>
+          window.onload = () => {
+            window.ui = SwaggerUIBundle({
+              url: '/api/api-docs.json',
+              dom_id: '#swagger-ui',
+              deepLinking: true,
+              presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+              layout: 'StandaloneLayout',
+            });
+          };
+        </script>
+      </body>
+    </html>
+  `);
+});
 
 app.use('/api', routes);
 
